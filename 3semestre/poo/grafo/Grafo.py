fda301162dc818ba9c92aa9f34e9925fc5f13416
Felipe Cerri <felipecamargocerri@gmail.com>
2025-07-12 21:53:58 -0300

    Atualizar pasta poo

--- 3semestre/poo/grafo/Grafo.py
+++ 3semestre/poo/grafo/Grafo.py
@@ -1,24 +1,55 @@
 import heapq
-INF = 99999
+INF = 999999
 
 class Grafo:
     def __init__(self):
         self.tamanho = 0
         self.grafo = {}
+        self.nomes = []
 
-    def adicionaCidade(self, cidade, **kwargs):
-        self.grafo[cidade] = kwargs
+    def adicionaCidade(self, cidade, vizinhos):
+        self.grafo[cidade] = vizinhos
+        self.nomes.append(cidade)
+        self.tamanho += 1
 
-    def djikstra(self, inicio, fim) :
+    def djikstra(self, inicio, fim):
         q = []
-        distancia = [INF]*self.tamanho
+        distancia = {}
+        processado = {}
+        anterior = {} # guarda o no anterior no caminho
 
-        for i in range(self.tamanho):
-            distancia[i] = INF
-            distancia[inicio] = 0
-            heapq.heappush(q, (0, inicio))
+        distancia[inicio] = 0
+        heapq.heappush(q, (0, inicio))
 
-            
+        while q:
+            dist_a, a = heapq.heappop(q)
+
+            if a in processado and processado[a]:
+                continue
+            processado[a] = True
+
+            for b, w in self.grafo.get(a, {}).items():  # (vizinho, peso)
+
+                if b not in distancia: #inicializa distancia se nao exsitir
+                    distancia[b] = INF
+
+                if distancia[a] + w < distancia[b]:
+                    distancia[b] = distancia[a] + w
+                    anterior[b] = a 
+                    heapq.heappush(q, (distancia[b], b))
+
+
+        # Reconstroi o caminho
+        caminho = []
+        atual = fim
+        while atual != inicio:
+            caminho.append(atual)
+            atual = anterior[atual]
+
+        caminho.reverse()
+
+        return distancia[fim], caminho
+
 
 
 
--- 3semestre/poo/grafo/main.py
+++ 3semestre/poo/grafo/main.py
@@ -1,1 +1,54 @@
+from Grafo import Grafo
 
+line = []
+lines = []
+count = 0
+vizinhos = {} 
+Cidades = Grafo()
+nomeCidade = []
+
+while True:
+    try:
+        line = input()
+        count += 1
+        lines.append(line)
+    except EOFError:
+        break
+
+for i in range(count):
+
+    if lines[i][0] == '\t': #cidade é destino
+        vizinho = lines[i].strip().split() 
+        vizinhos[vizinho[0]] = int(vizinho[1])
+    else: 
+        if vizinhos != {}:
+            Cidades.adicionaCidade(nomeCidade, vizinhos)
+        vizinhos = {}       
+    
+    if lines[i][0] != '\t': #cidade origem
+        nomeCidade = lines[i].strip()
+
+if nomeCidade is not None: #add a ultima cidade
+    Cidades.adicionaCidade(nomeCidade, vizinhos)
+    
+
+tam = Cidades.tamanho
+for i in range(tam):
+    for j in range(tam):
+        if i == j: 
+            continue
+
+        distancia, caminho = Cidades.djikstra(Cidades.nomes[i], Cidades.nomes[j])
+
+        print(Cidades.nomes[i] + " para " + Cidades.nomes[j])
+        print("\tDistancia: " + format(distancia) + ",0")
+        print("\tCaminho: ", end="")
+        for cidade in caminho:
+            print(" --> " + cidade, end="")
+            
+        print()
+
+    print("---------------------------------------------")
+
+    
+            
