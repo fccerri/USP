be3a3861baf2dc383b3595e90d2e38f92e9514c6
fccerri <felipecamargocerri@gmail.com>
2025-10-07 22:51:18 -0300

    finalizar trab02 alg

--- 4semestre/algoritmosAvancados/trab02/main.cpp
+++ 4semestre/algoritmosAvancados/trab02/main.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-
 struct sistema {
     string nome;
     double x;
     double y;
+    int id;
 
     static double calc_dist(sistema s1, sistema s2) {
         double dx = s1.x - s2.x;
@@ -35,15 +35,17 @@ struct aresta {
     double peso;
 
     aresta(sistema s1, sistema s2, double peso) {
-        this->s1 = s1; this->s2 = s2; this->peso = peso;  
-    }
-    aresta() = default; //permite nao inicializar as variaves
-
-    static bool comparaPeso(const aresta &a, const aresta &b){ //para ordenar por peso
-        return a.peso < b.peso;
+        this->s1 = s1.id < s2.id ? s1 : s2; //garante que s1.id < s2.id
+        this->s2 = s1.id > s2.id ? s1 : s2;  //garante que s2.id > s1.id 
+        this->peso = peso;  
     }
 
-    //assume que s1.nome < s2.nome
+    aresta() { //permite nao inicializar as variaves evitando core dumped
+        this->s1 = sistema{"", 0.0, 0.0, -1};
+        this->s2 = sistema{"", 0.0, 0.0, -1};
+        this->peso = DBL_MAX;
+    }
+
     static bool comparaNome(const aresta &a, const aresta &b) { //para ordenar por nome
         if (a.s1.nome < b.s1.nome) {
         return true; // a vem antes de b
@@ -53,8 +55,28 @@ struct aresta {
         }
 
         // se a.s1.nome == b.s1.nome, desempata pelo segundo sistema
-        return a.s2.nome < b.s2.nome; //(nao sera necessario para os problemas propostos)
+        return a.s2.nome < b.s2.nome; 
     }
+
+    static bool comparaID(const aresta &a, const aresta &b) { //compara aresta por id
+       if (a.s1.id < b.s1.id) {
+        return true; // a vem antes de b
+        }
+        if (a.s1.id > b.s1.id) {
+            return false; // a vem depois de b
+        }
+
+        // se a.s1.id == b.s1.id, desempata pelo segundo sistema
+        return a.s2.id < b.s2.id; 
+    }
+
+    static bool comparaPeso(const aresta &a, const aresta &b){ //para ordenar por peso
+        if (a.peso != b.peso) //--- Essas linhas corrigem a saida mal formatada do passo 4
+            return a.peso < b.peso;
+        else
+            return comparaID(a, b); //desempata por ID (ordem de entrada)
+    }
+
 
     static vector<aresta> geraArestas(const vector<sistema> &sistemas, double tensaoMaxima) {
         //gera as arestas
@@ -63,15 +85,8 @@ struct aresta {
             for (int j = i + 1; j < sistemas.size(); ++j) {
                 double dist = sistema::calc_dist(sistemas[i], sistemas[j]);
                 if (dist <= tensaoMaxima) {
-                    //garante ordem alfabetica
-                    if (sistema::comparaNome(sistemas[i], sistemas[j])) 
-                        arestas.push_back({sistemas[i], sistemas[j], dist}); 
-                    else 
-                        arestas.push_back({sistemas[j], sistemas[i], dist});
-                        
-
+                    arestas.push_back({sistemas[i], sistemas[j], dist}); 
                 }
-                    
             }
         }
 
@@ -79,10 +94,16 @@ struct aresta {
     }
 };
 
+ostream& operator<<(ostream& os, const aresta& a) { //para printar arestas
+    os  << a.s1.nome << ", " << a.s2.nome << ", " << fixed << setprecision(2) << a.peso;
+    return os;
+}
+
+
 struct DSU { //disjoint set uniom
     map<string, string> pai; //conjuntos
     
-    DSU(vector<sistema> sistemas) {
+    DSU(vector<sistema> sistemas) { //inicializa
         for (auto s : sistemas) {
             pai[s.nome] = s.nome;
         }
@@ -104,7 +125,7 @@ struct DSU { //disjoint set uniom
         }
     }
 
-    vector<aresta> kruskal(const vector<sistema> &sistemas, double tensaoMaxima) { //algoritmos de resolucao da parte 1
+    vector<aresta> kruskal(const vector<sistema> &sistemas, double tensaoMaxima) { //algoritmo de resolucao da parte 1
         vector<aresta> arestas = aresta:: geraArestas(sistemas, tensaoMaxima); //gera arestas do grafo
         sort(arestas.begin(), arestas.end(), aresta::comparaPeso); //ordena por peso 
         
@@ -117,108 +138,102 @@ struct DSU { //disjoint set uniom
             }
         }
 
-        sort(mst.begin(), mst.end(), aresta::comparaNome); //deixa em ordem alfabetica 
-        return mst;
-    }
+        // sort(mst.begin(), mst.end(), aresta::comparaNome); //deixa em ordem de entrada
 
-    
+        return mst;
+    }    
 
 };
 
 struct CPP { //closest pair of points
     vector<sistema> sx, sy; //sistemas odenados por x e por y
-    sistema pontoRessonante;
     const double tensaoMaxima;
 
-
-    CPP (vector<sistema> sistermas, double tensaoMaxima): tensaoMaxima(tensaoMaxima) {
+    CPP (vector<sistema> sistermas, double tensaoMaxima): tensaoMaxima(tensaoMaxima) { //inicializa 
         sx = sistermas;
         sort(sx.begin(), sx.end(), sistema::comparaX);
         sy = sistermas;
         sort(sy.begin(), sy.end(), sistema::comparaY);
     }
 
-    aresta achaPontoRessonante() {
+    aresta achaPontoRessonante() { //algoritmo para resolucao da parte 2
         int inicio = 0; 
         int fim = sx.size() - 1;
 
-        aresta min = achaRecursivo(inicio, fim);
+        aresta min = achaRecursivo(inicio, fim, sy);
 
         return min;
     }
 
     private:
-        aresta achaRecursivo(int inicio, int fim) {
+        aresta setArestaMin(aresta a, aresta b) { //compara duas aresta e retorna a com menor distancia
+            if (a.peso != b.peso) { //retorna com base no peso
+                return a.peso < b.peso ? a : b;
+            }
+            else
+                return aresta::comparaID(a,b) ? a : b; //desempata por id
+                
+        }
+
+        //funcao recursiva para o algoritmo da parte 2
+        aresta achaRecursivo(int inicio, int fim, vector<sistema>& sy_ordenado) {
             int meio = (inicio + fim) / 2;
-            if (inicio - fim <= 3)
+            if (fim - inicio <= 3)
                 return brutaMinimo(inicio, fim); //faz o brute force
 
-            aresta minEsq = achaRecursivo(inicio, meio);
-            aresta minDir = achaRecursivo(meio + 1, fim);
-   
-            aresta arestaMin =  minEsq.peso < minDir.peso ?  minEsq : minDir; //acha o minmo da direita/esq
+            vector<sistema> sy_esq, sy_dir; //subvtores de sistemas ordenados em y para otimizacao
+            double medianaX = sx[meio].x; //ponto mediana de x
+            
 
-            vector<sistema> strip; //contem os pontos em que dsitancia de x á mediana de x < min
-
-            sistema medianaX = sx[meio];
-            for (int i = inicio; i < fim; i++) {
-                if (sistema::calc_dist(medianaX, sx[i]) < arestaMin.peso)
-                    strip.push_back(sx[i]);
+            for (auto& s : sy_ordenado) { //otimiza a ordenacao
+                if (s.x <= medianaX) {
+                    sy_esq.push_back(s);
+                } else {
+                    sy_dir.push_back(s);
+                }
             }
 
-            sort(strip.begin(), strip.end(), sistema::comparaY); //precisa utilizar sy pra otimizar
+            aresta minEsq = achaRecursivo(inicio, meio, sy_esq); //continua recursao na esquerda
+            aresta minDir = achaRecursivo(meio + 1, fim, sy_dir); //continua recursao na direita
 
-            for (int i = 0; i < strip.size(); i++) {
-                //verifica vizinhos da direita
-                for (int j = 1; j <= 7 && i+j < strip.size(); i++ ) { //usa a propriedade de que só precisa verificar 7 vizinhos
-                    double peso = sistema::calc_dist(strip[i+j], strip[i]);
-                    aresta novoMin = aresta(strip[i+j], strip[i], peso);
-                    arestaMin = arestaMin.peso < novoMin.peso ? arestaMin : novoMin;
-                }
-                //verifica vizinhos da esquerda
-                for (int j = 1; j <= 7 && i-j > 0; i++ ) { //usa a propriedade de que só precisa verificar 7 vizinhos
-                    double peso = sistema::calc_dist(strip[i-j], strip[i]);
-                    aresta novoMin = aresta(strip[i-j], strip[i], peso);
-                    arestaMin = arestaMin.peso < novoMin.peso ? arestaMin : novoMin;
-                }                         
+            aresta arestaMin = setArestaMin(minEsq, minDir); //acha o minmo da direita/esq
+
+            vector<sistema> strip; //contem os pontos em que dsitancia de x á mediana de x < min (nao considera distancia absoluta apenas em x)
+            
+            //mais rapido que ordenar strip apos monta-la (otimiza de nlog^2n para nlogn)
+            for (auto s : sy_ordenado) { //acha strip utilizando vetor previamente ordenado por y e ajustado com base na mediana
+                if (abs(s.x - medianaX) < arestaMin.peso) 
+                    strip.push_back(s);
+            }
+
+            for (int i = 0; i < strip.size(); i++) { //usa propriedade de apenas olhar 7 vizinhos
+                for (int j = i + 1; j < strip.size() && j <= i + 7; j++) {                        
+                    double peso = sistema::calc_dist(strip[i], strip[j]);
+                    aresta novoMin = aresta(strip[i], strip[j], peso);
+                    arestaMin = setArestaMin(arestaMin, novoMin);
+                }                       
             }
             
             return arestaMin;
         }
 
-        aresta brutaMinimo(int inicio, int fim) {
-            double distancia_minima = DBL_MAX;
-            aresta aresta_minima;
-            aresta_minima.peso = distancia_minima;
-                
-            //itera pelos sistemas (3 apenas)
+         aresta brutaMinimo(int inicio, int fim) {
+            aresta arestaMin; //inicializado com peso = DBL_MAX (caso inicio = fim)
+            
             for (int i = inicio; i <= fim; ++i) {
                 for (int j = i + 1; j <= fim; ++j) {
-                
-                    double dist_atual = sistema::calc_dist(sx[i], sx[j]);
-                    // se a distancia for a minima ate o momento
-                    if (dist_atual < distancia_minima) {
-                        distancia_minima = dist_atual; //att distancia minima
-                    
-                        // cria a nova aresta minimoa, garantindo a ordem alfabetica dos sistemas
-                        if (sistema::comparaNome(sx[i], sx[j])) 
-                            aresta_minima = aresta(sx[i], sx[j], dist_atual);
-                        else 
-                            aresta_minima = aresta(sx[j], sx[i], dist_atual);
-                    }
+                    double distAtual = sistema::calc_dist(sx[i], sx[j]);
+                    aresta candidata = aresta(sx[i], sx[j], distAtual);
+
+                    arestaMin = setArestaMin(arestaMin, candidata);
                 }
             }
-
-            return aresta_minima;
+            return arestaMin;
         }
 
     
 };
 
-ostream& operator<<(ostream& os, const aresta& a) { //para printar arestas
-    os  << a.s1.nome << ", " << a.s2.nome << ", " << a.peso << fixed << setprecision(2);
-    return os;
-}
 
 int main () {
     int x; cin >> x;
@@ -228,8 +243,9 @@ int main () {
 
         vector<sistema> sistemas; //vetor de sistemas
 
+        int id_counter = 0;
         for (int j = 0; j < importantes; j++ ) { //le os sistemas importantes
-            sistema s; cin >> s.nome >> s.x >> s.y;
+            sistema s; cin >> s.nome >> s.x >> s.y; s.id = id_counter++;
             sistemas.push_back(s);
         }
     
@@ -237,7 +253,7 @@ int main () {
         vector<aresta> mst = d.kruskal(sistemas, tensaoMaxima); //resolve parte 1
 
         for (int j = importantes; j <  n; j++ ) { //le o restante dos sistemas
-            sistema s; cin >> s.nome >> s.x >> s.y;
+            sistema s; cin >> s.nome >> s.x >> s.y; s.id = id_counter++;
             sistemas.push_back(s);
             
         }
@@ -247,10 +263,8 @@ int main () {
         }
 
         CPP c = CPP(sistemas, tensaoMaxima);
-        aresta ressonancia = c.achaPontoRessonante();
-        cout << "Ponto de ressonância: " << ressonancia << endl; //printa parte 2
-
-        if (i != x- 1) cout << endl;
-
+        aresta ressonancia = c.achaPontoRessonante(); //resolve parte 2
+        
+        cout << "Ponto de Ressonância: " << ressonancia << endl << endl; //printa parte 2
     }
 }
