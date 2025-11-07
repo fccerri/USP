b4d5b5149efd2588d3a498f5da03e0c93741c6da
fccerri <felipecamargocerri@gmail.com>
2025-11-07 10:23:02 -0300

    Implement ex11

---
+++ 4semestre/algoritmosAvancados/ex11/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 05/11/2025 14:51:50 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Exercício 11: Contar Moléculas
---
+++ 4semestre/algoritmosAvancados/ex11/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+struct DSU { //disjoint set uniom
+    map<int, int> pai; //conjuntos
+    int conjuntos; //qtd de conjuntos
+    
+    DSU(int atomos): conjuntos(atomos) {};
+
+    void insert_bond(int n1, int n2) {
+        make_set(n1);
+        make_set(n2);
+        if (unite(n1, n2) == true)
+            conjuntos--;5
+    }
+
+    void make_set(int n) {
+        if (pai.find(n) == pai.end()) { //elemento não existe
+            pai[n] = n; //cria elemento
+        }    
+    }
+
+    int find(int id) {
+        if (pai[id] == id)
+            return pai[id];
+        else
+            return pai[id] = find(pai[id]); //compressao de caminho (otimizacao) 
+    }
+
+    //une os conjuntos de dois sistemas
+    bool unite(int id1, int id2) {
+        int raiz1 = find(id1);
+        int raiz2 = find(id2);
+        if (raiz1 != raiz2) {
+            pai[raiz1] = raiz2;
+            return true; //uniu
+        }
+        return false; //nao uniu (ja eram parte do mesmo conjunto)
+    }
+};
+
+int main () {
+    int x; cin >> x;
+    vector<int> saida(x);
+    while (x--) {
+        int n, m; cin >> n >> m;
+        DSU moleculas(n);
+        for (int i = 0; i < m; i++) {
+            int n1, n2; cin >>n1 >> n2;
+            moleculas.insert_bond(n1, n2);
+        }
+        cout << moleculas.conjuntos << endl;
+    }
+
+}
